519bc7c8ee3d238a721398d9a45cae6ef5bb52b6
CGraczyk <your.email@example.com>
2025-12-02 13:51:23 +0100

    Error handling including async dependency and some skaffold config improvements

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,21 @@
+import { CustomError } from "./custom-error.js";
+
+export class DatabaseConnectionError extends CustomError {
+  serialzeErrors(): { message: string; field?: string; }[] {
+    throw new Error("Method not implemented.");
+  }
+  statusCode = 500;
+  reason = "Error Connecting to Database."
+  constructor() {
+    super("Error Connecting to Database.");
+
+    // Only because extending built-in Class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason}
+    ];
+  }
+}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error.js";
+import { DatabaseConnectionError } from "../errors/database-connection-error.js";
 
 const router = express.Router();
 
@@ -13,17 +15,17 @@ router.post("/api/users/signup", [
       .isLength({min:4, max:20})
       .withMessage("Password must be between 4 and 20 characters")
 ],
-(req: Request, res: Response) => {
+ async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
-    return res.status(400).send(errors.array());
+    throw new RequestValidationError(errors.array());
   }
 
-  const { email, password } = req.body;
+  console.log("Creating a user ...!");
+  throw new DatabaseConnectionError();
 
-  console.log("Creating a user ....");
-  res.send({})
+  res.send({});
   // new User({ email, passowrd })
 
 });
