ce6d6c5aa546e2535e6a1f58e1cc47fba2f94dc0
CGraczyk <your.email@example.com>
2025-12-02 09:45:40 +0100

    Setting up routers for authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import json from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user.js";
+import { signinRouter } from "./routes/signin.js";
+import { signoutRouter } from "./routes/signout.js";
+import { signupRouter } from "./routes/signup.js";
+
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!");
