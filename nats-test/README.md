edcea1c31b6ee2293d1f57431b9b98176e5ccb3a
Cgraczyk <75727792+CGraczyk@users.noreply.github.com>
2026-04-17 12:29:52 +0200

    Order Service for Tickets Create the Cross Plattform Data handling and Ordering service including front end.

---
+++ nats-test/README.md
@@ -0,0 +1,8 @@
+##commands for testing
+
+kubectl port-forward nats-depl-6cc65c7f5d-5m2cq 4222:4222
+
+npm run publish from second terminal
+npm run listen from third terminal
+
+enter rs in running process to restart.
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,24 @@
+import nats from "node-nats-streaming";
+import { connect } from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
+console.clear();
+
+const stan = connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({ id: "123", title: "concert", price: 123 });
+});
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
